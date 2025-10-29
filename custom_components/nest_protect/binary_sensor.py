bffc6ed9b5ae4d214f67516b737c3033aa13e387
Jastro <Jan.Korte@gmx.net>
2025-10-29 12:37:30 +0100

    Update binary_sensor.py

--- custom_components/nest_protect/binary_sensor.py
+++ custom_components/nest_protect/binary_sensor.py
@@ -1,231 +1,77 @@
-"""Binary sensor platform for Nest Protect."""
+"""Binary sensor platform for Nest Protect (Smoke/CO)."""
 
 from __future__ import annotations
+from homeassistant.components.binary_sensor import BinarySensorEntity
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.core import HomeAssistant
 
-from collections.abc import Callable
-from dataclasses import dataclass
-from typing import Any
-
-from homeassistant.components.binary_sensor import (
-    BinarySensorDeviceClass,
-    BinarySensorEntity,
-    BinarySensorEntityDescription,
-)
-from homeassistant.helpers.entity import EntityCategory
-
-from . import HomeAssistantNestProtectData
 from .const import DOMAIN
-from .entity import NestDescriptiveEntity
+from .sdm_client import sdm_execute_command, sdm_get_device
 
 
-@dataclass
-class NestProtectBinarySensorDescriptionMixin:
-    """Define an entity description mixin for binary sensor entities."""
-
-    value_fn: Callable[[Any], bool | None]
-
-
-@dataclass
-class NestProtectBinarySensorDescription(
-    BinarySensorEntityDescription, NestProtectBinarySensorDescriptionMixin
-):
-    """Class to describe a Nest Protect binary sensor."""
-
-    wired_only: bool = False
+async def async_setup_entry(hass: HomeAssistant, entry, async_add_entities: AddEntitiesCallback):
+    """Set up binary sensors."""
+    data = hass.data[DOMAIN][entry.entry_id]
+    devices = data.get("sdm_devices", {}).get("devices", [])
+    entities = []
+    for dev in devices:
+        traits = dev.get("traits", {})
+        if any(k.endswith("SmokeAlarm") or k.endswith("CarbonMonoxide") for k in traits):
+            entities.append(NestProtectBinarySensor(hass, entry.entry_id, dev))
+    async_add_entities(entities)
 
 
-def _state_not_zero(state: Any) -> bool | None:
-    """Return True when the state is a non-zero value."""
+class NestProtectBinarySensor(BinarySensorEntity):
+    """Representation of a Nest Protect smoke/CO alarm."""
 
-    if state is None:
-        return None
-
-    return state != 0
-
-
-def _state_is_false(state: Any) -> bool | None:
-    """Return True when the state evaluates to False."""
-
-    if state is None:
-        return None
-
-    return not bool(state)
-
-
-def _bool_or_none(state: Any) -> bool | None:
-    """Return a boolean representation of the state when available."""
-
-    if state is None:
-        return None
-
-    return bool(state)
-
-
-BINARY_SENSOR_DESCRIPTIONS: list[BinarySensorEntityDescription] = [
-    NestProtectBinarySensorDescription(
-        key="co_status",
-        name="CO Status",
-        device_class=BinarySensorDeviceClass.CO,
-        value_fn=_state_not_zero,
-    ),
-    NestProtectBinarySensorDescription(
-        key="smoke_status",
-        name="Smoke Status",
-        device_class=BinarySensorDeviceClass.SMOKE,
-        value_fn=_state_not_zero,
-    ),
-    NestProtectBinarySensorDescription(
-        key="heat_status",
-        name="Heat Status",
-        device_class=BinarySensorDeviceClass.HEAT,
-        value_fn=_state_not_zero,
-    ),
-    NestProtectBinarySensorDescription(
-        key="component_speaker_test_passed",
-        name="Speaker Test",
-        device_class=BinarySensorDeviceClass.PROBLEM,
-        entity_category=EntityCategory.DIAGNOSTIC,
-        icon="mdi:speaker-wireless",
-        value_fn=_state_is_false,
-    ),
-    NestProtectBinarySensorDescription(
-        key="battery_health_state",
-        name="Battery Health",
-        device_class=BinarySensorDeviceClass.BATTERY,
-        entity_category=EntityCategory.DIAGNOSTIC,
-        value_fn=_bool_or_none,
-    ),
-    NestProtectBinarySensorDescription(
-        key="is_online",
-        name="Online",
-        device_class=BinarySensorDeviceClass.CONNECTIVITY,
-        entity_category=EntityCategory.DIAGNOSTIC,
-        value_fn=_bool_or_none,
-    ),
-    NestProtectBinarySensorDescription(
-        key="component_smoke_test_passed",
-        name="Smoke Test",
-        device_class=BinarySensorDeviceClass.PROBLEM,
-        entity_category=EntityCategory.DIAGNOSTIC,
-        icon="mdi:smoke",
-        value_fn=_state_is_false,
-    ),
-    NestProtectBinarySensorDescription(
-        key="component_co_test_passed",
-        name="CO Test",
-        device_class=BinarySensorDeviceClass.PROBLEM,
-        entity_category=EntityCategory.DIAGNOSTIC,
-        icon="mdi:molecule-co",
-        value_fn=_state_is_false,
-    ),
-    NestProtectBinarySensorDescription(
-        key="component_wifi_test_passed",
-        name="WiFi Test",
-        device_class=BinarySensorDeviceClass.PROBLEM,
-        entity_category=EntityCategory.DIAGNOSTIC,
-        icon="mdi:wifi",
-        value_fn=_state_is_false,
-    ),
-    NestProtectBinarySensorDescription(
-        key="component_led_test_passed",
-        name="LED Test",
-        device_class=BinarySensorDeviceClass.PROBLEM,
-        entity_category=EntityCategory.DIAGNOSTIC,
-        icon="mdi:led-off",
-        value_fn=_state_is_false,
-    ),
-    NestProtectBinarySensorDescription(
-        key="component_pir_test_passed",
-        name="PIR Test",
-        device_class=BinarySensorDeviceClass.PROBLEM,
-        entity_category=EntityCategory.DIAGNOSTIC,
-        icon="mdi:run",
-        value_fn=_state_is_false,
-    ),
-    NestProtectBinarySensorDescription(
-        key="component_buzzer_test_passed",
-        name="Buzzer Test",
-        device_class=BinarySensorDeviceClass.PROBLEM,
-        entity_category=EntityCategory.DIAGNOSTIC,
-        icon="mdi:alarm-bell",
-        value_fn=_state_is_false,
-    ),
-    # Disabled for now, since it seems like this state is not valid
-    # NestProtectBinarySensorDescription(
-    #     key="component_heat_test_passed",
-    #     name="Heat Test",
-    #     device_class=BinarySensorDeviceClass.PROBLEM,
-    #     entity_category=EntityCategory.DIAGNOSTIC,
-    #     icon="mdi:fire",
-    #     value_fn=lambda state: not state
-    # ),
-    NestProtectBinarySensorDescription(
-        key="component_hum_test_passed",
-        name="Humidity Test",
-        device_class=BinarySensorDeviceClass.PROBLEM,
-        entity_category=EntityCategory.DIAGNOSTIC,
-        icon="mdi:water-percent",
-        value_fn=_state_is_false,
-    ),
-    NestProtectBinarySensorDescription(
-        key="removed_from_base",
-        name="Removed from Base",
-        device_class=BinarySensorDeviceClass.PROBLEM,
-        entity_category=EntityCategory.DIAGNOSTIC,
-        icon="mdi:tray-remove",
-        value_fn=_bool_or_none,
-    ),
-    NestProtectBinarySensorDescription(
-        key="auto_away",
-        name="Occupancy",
-        device_class=BinarySensorDeviceClass.OCCUPANCY,
-        wired_only=True,
-        value_fn=_state_is_false,
-    ),
-    NestProtectBinarySensorDescription(
-        key="line_power_present",
-        name="Line Power",
-        device_class=BinarySensorDeviceClass.POWER,
-        entity_category=EntityCategory.DIAGNOSTIC,
-        wired_only=True,
-        value_fn=_bool_or_none,
-    ),
-]
-
-
-async def async_setup_entry(hass, entry, async_add_devices):
-    """Set up the Nest Protect binary sensors from a config entry."""
-
-    data: HomeAssistantNestProtectData = hass.data[DOMAIN][entry.entry_id]
-    entities: list[NestProtectBinarySensor] = []
-
-    SUPPORTED_KEYS: dict[str, NestProtectBinarySensorDescription] = {
-        description.key: description for description in BINARY_SENSOR_DESCRIPTIONS
-    }
-
-    for device in data.devices.values():
-        for key in device.value:
-            if description := SUPPORTED_KEYS.get(key):
-                # Not all entities are useful for battery powered Nest Protect devices
-                if description.wired_only and device.value["wired_or_battery"] != 0:
-                    continue
-
-                entities.append(
-                    NestProtectBinarySensor(
-                        device, description, data.areas, data.client
-                    )
-                )
-
-    async_add_devices(entities)
-
-
-class NestProtectBinarySensor(NestDescriptiveEntity, BinarySensorEntity):
-    """Representation of a Nest Protect Binary Sensor."""
-
-    entity_description: NestProtectBinarySensorDescription
+    def __init__(self, hass: HomeAssistant, entry_id: str, device: dict):
+        self.hass = hass
+        self._entry_id = entry_id
+        self._device = device
+        self._device_name = device.get("name")
+        self._attr_name = device.get("customName") or device.get("name")
+        self._attr_unique_id = self._device_name
 
     @property
-    def is_on(self) -> bool | None:
-        """Return the state of the sensor."""
-        state = self.bucket.value.get(self.entity_description.key)
-        return self.entity_description.value_fn(state)
+    def is_on(self) -> bool:
+        """Return true if alarm is active."""
+        traits = self._device.get("traits", {})
+        for trait_key, trait_value in traits.items():
+            if trait_key.endswith("SmokeAlarm"):
+                state = trait_value.get("alarmState") or trait_value.get("state", {}).get("alarmState")
+                return state in ("SMOKE", "FIRE", "ALARM")
+            if trait_key.endswith("CarbonMonoxide"):
+                state = trait_value.get("alarmState") or trait_value.get("state", {}).get("alarmState")
+                return state in ("CO_ALARM", "ALARM")
+        return False
+
+    @property
+    def extra_state_attributes(self):
+        data = self.hass.data[DOMAIN][self._entry_id]
+        attrs = {"restricted": data.get("restricted", False)}
+        if data.get("restricted_reason"):
+            attrs["restricted_reason"] = data["restricted_reason"]
+        return attrs
+
+    async def async_update(self):
+        """Refresh device state from SDM."""
+        data = self.hass.data[DOMAIN][self._entry_id]
+        if not data.get("device_access"):
+            return
+        session = async_get_clientsession(self.hass)
+        try:
+            updated = await sdm_get_device(session, data["access_token"], self._device_name)
+            if updated:
+                self._device = updated
+        except Exception:
+            pass
+
+    async def async_silence(self):
+        """Send Silence command."""
+        data = self.hass.data[DOMAIN][self._entry_id]
+        if not data.get("device_access"):
+            raise RuntimeError("Device Access not configured")
+        session = async_get_clientsession(self.hass)
+        cmd = {"command": "sdm.devices.commands.SmokeAlarm.Silence", "params": {}}
+        await sdm_execute_command(session, data["access_token"], self._device_name, cmd)
