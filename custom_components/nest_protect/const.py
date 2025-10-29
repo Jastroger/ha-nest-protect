f1d38033297cf9800d3eae08798e0a3f72c1dfe0
Jastro <Jan.Korte@gmx.net>
2025-10-29 11:36:22 +0100

    Clean up OAuth scope comments in const.py
    Removed comments about deprecated OAuth scopes.

--- custom_components/nest_protect/const.py
+++ custom_components/nest_protect/const.py
@@ -15,21 +15,6 @@ ATTRIBUTION: Final = "Data provided by Google"
 # Google OAuth authorize endpoint
 OAUTH_AUTHORIZE_URL: Final = "https://accounts.google.com/o/oauth2/v2/auth"
 
-# Wichtig:
-# Ursprünglich wollten wir zwei Scopes anfragen:
-#   - https://www.googleapis.com/auth/nest-account.readonly
-#   - https://www.googleapis.com/auth/userinfo.email
-#
-# Der Scope "nest-account.readonly" ist aber für normale/neue Google Cloud Projekte
-# nicht mehr freigeschaltet. Google antwortet dann mit:
-#   invalid_scope
-#
-# Für unseren Flow reicht faktisch ein normales Google OAuth Token aus,
-# plus userinfo.email zur Account-Zuordnung. Den Rest erledigen wir
-# dann über den inoffiziellen Nest-Login-Proxy in client.py.
-#
-# Deshalb reduzieren wir die Scopes hier auf userinfo.email.
-#
 OAUTH_SCOPES: Final = [
     "https://www.googleapis.com/auth/userinfo.email",
 ]
